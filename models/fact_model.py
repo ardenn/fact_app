0c131454de3ff3558f4697caf3fb7f41c1d0a2ea
ardenn <mc_alila@outlook.com>
2017-11-09 16:36:23 +0000

    Added generate_token and verify_token methods to User

--- models/fact_model.py
+++ models/fact_model.py
@@ -2,11 +2,30 @@ from models.base_model import *
 from peewee import CharField, BooleanField, TextField, DateTimeField, ForeignKeyField
 
 from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired
+
+SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
 
 
 class User(BaseModel):
     name = CharField(40)
     password = CharField(250)
+
+    def generate_token(self, expiry=600):
+        t = Serializer(secret_key=SECRET_KEY, expires_in=expiry)
+        return t.dumps({"id": self.id})
+
+    @staticmethod
+    def verify_token(token):
+        t = Serializer(SECRET_KEY)
+        try:
+            data = t.loads(token)
+        except SignatureExpired:
+            return None  # Token expired
+        except BadSignature:
+            return None  # Token Invalid
+        user = User.get(User.id == data["id"])
+        return user
 
 
 class FactModel(BaseModel):
